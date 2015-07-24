7c6c8b52f37b66694cf51d87f6629ee95d8c4e96
Ahmet Kaşif <ahmetkasif1995@gmail.com>
2015-07-24 16:00:15 +0300

    Site Overhaul
    - Site is getting rebuilt by new templates and php structure

---
+++ about.php
@@ -0,0 +1,84 @@
+<!DOCTYPE HTML>
+<!--
+	ZeroFour template by HTML5 UP
+	html5up.net | @n33co
+
+	Configuration by KittenCode Web Dev. Team
+	www.kittencode.github.io
+	Licensed under MIT License
+-->
+
+<?php
+	$dev_name = 'Kittencode';
+	$project1 = 'Kittenmaxit';
+	$project2 = 'Whoops';
+	$project3 = 'Eu4-local-tr';
+	$project4 = 'Kittenchess';
+?>
+
+<html>
+	<head>
+		<title><?php echo $dev_name;?></title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+		<div id="page-wrapper">
+			<!-- Header -->
+			<div id="header-wrapper">
+				<div class="container">
+					<!-- Header -->
+						<header id="header">
+							<div class="inner">
+								<!-- Logo -->
+								<h1><?php echo $dev_name;?></h1>
+								<!-- Nav -->
+								<nav id="nav">
+									<ul>
+										<li class="current_page_item"><a href="index.php">Home</a></li>
+										<li>
+											<a href="#">Projects</a>
+											<ul>
+												<li><a href="kittenmaxit.php"><?php echo $project1;?></a></li>
+												<li><a href="whoops.php"><?php echo $project2;?></a></li>
+												<li>
+													<a href="eu4localtr.php"><?php echo $project3;?></a>
+													<ul>
+														<li><a href="#">Lorem ipsum dolor</a></li>
+														<li><a href="#">Phasellus consequat</a></li>
+														<li><a href="#">Magna phasellus</a></li>
+														<li><a href="#">Etiam dolore nisl</a></li>
+													</ul>
+												</li>
+												<li><a href="kittenchess.php"><?php echo $project4;?></a></li>
+											</ul>
+										</li>
+										<li><a href="blog.php">Blog</a></li>
+										<li><a href="about.php">About</a></li>
+									</ul>
+								</nav>
+							</div>
+						</header>
+					</div>
+
+
+
+			</div>
+		</div>
+
+		<!-- Scripts -->
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+		
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE HTML>
+<!--
+	ZeroFour template by HTML5 UP
+	html5up.net | @n33co
+
+	Configuration by KittenCode Web Dev. Team
+	www.kittencode.github.io
+	Licensed under MIT License
+-->
+
+<?php
+	$dev_name = 'Kittencode';
+	$project1 = 'Kittenmaxit';
+	$project2 = 'Whoops';
+	$project3 = 'Eu4-local-tr';
+	$project4 = 'Kittenchess';
+?>
+
+<html>
+	<head>
+		<title><?php echo $dev_name;?></title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header-wrapper">
+					<div class="container">
+
+						<!-- Header -->
+							<header id="header">
+								<div class="inner">
+
+									<!-- Logo -->
+										<h1><?php echo $dev_name;?></h1>
+
+									<!-- Nav -->
+										<nav id="nav">
+											<ul>
+												<li class="current_page_item"><a href="index.php">Home</a></li>
+												<li>
+													<a href="#">Projects</a>
+													<ul>
+														<li><a href="kittenmaxit.php"><?php echo $project1;?></a></li>
+														<li><a href="whoops.php"><?php echo $project2;?></a></li>
+														<li>
+															<a href="eu4localtr.php"><?php echo $project3;?></a>
+															<ul>
+																<li><a href="#">Lorem ipsum dolor</a></li>
+																<li><a href="#">Phasellus consequat</a></li>
+																<li><a href="#">Magna phasellus</a></li>
+																<li><a href="#">Etiam dolore nisl</a></li>
+															</ul>
+														</li>
+														<li><a href="kittenchess.php"><?php echo $project4;?></a></li>
+													</ul>
+												</li>
+												<li><a href="blog.php">Blog</a></li>
+												<li><a href="about.php">About</a></li>
+											</ul>
+										</nav>
+
+								</div>
+							</header>
+
+						<!-- Banner -->
+							<div id="banner">
+								<h2><strong>ZeroFour:</strong> A free responsive site template
+								<br />
+								built on HTML5 and CSS3 by <a href="http://html5up.net">HTML5 UP</a></h2>
+								<p>Does this statement make you want to click the big shiny button?</p>
+								<a href="#" class="button big icon fa-check-circle">Yes it does</a>
+							</div>
+
+					</div>
+				</div>
+
+			<!-- Main Wrapper -->
+				<div id="main-wrapper">
+					<div class="wrapper style1">
+						<div class="inner">
+
+							<!-- Feature 1 -->
+							<section class="container box feature1">
+								<div class="row">
+									<div class="12u">
+										<header class="first major">
+											<h2>Our Projects</h2>
+										</header>
+									</div>
+								</div>
+								<div class="row">
+									<div class="4u 12u(mobile)">
+										<section>
+											<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+											<header class="second icon fa-user">
+												<h3>First Project</h3>
+												<p>And a subtitle</p>
+											</header>
+										</section>
+									</div>
+									<div class="4u 12u(mobile)">
+										<section>
+											<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+											<header class="second icon fa-user">
+												<h3>Second Project</h3>
+												<p>And another subtitle</p>
+											</header>
+										</section>
+									</div>
+									<div class="4u 12u(mobile)">
+										<section>
+											<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+											<header class="second icon fa-user">
+												<h3>Third Project</h3>
+												<p>And yes, a subtitle</p>
+											</header>
+										</section>
+									</div>
+								</div>
+							</section>
+						</div>
+					</div>
+					<div class="wrapper style2">
+						<div class="inner">
+							<div class="container">
+								<div class="row">
+									<div class="8u 12u(mobile)">
+
+									<!-- Article list -->
+										<section class="box article-list">
+												<h2 class="icon fa-file-text-o">Recent Posts</h2>
+
+											<!-- Excerpt -->
+												<article class="box excerpt">
+													<a href="#" class="image left"><img src="images/pic04.jpg" alt="" /></a>
+													<div>
+														<header>
+															<span class="date">July 24</span>
+															<h3><a href="#">Turkish Localization is underway</a></h3>
+														</header>
+														<p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus
+														semper mod quisturpis nisi consequat etiam lorem. Phasellus quam turpis,
+														feugiat et sit amet ornare in, hendrerit in lectus semper mod quis eget mi dolore.</p>
+													</div>
+												</article>
+
+											<!-- Excerpt -->
+												<article class="box excerpt">
+													<a href="#" class="image left"><img src="images/pic05.jpg" alt="" /></a>
+													<div>
+														<header>
+															<span class="date">July 18</span>
+															<h3><a href="#">Happy Wednesday</a></h3>
+														</header>
+														<p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus
+														semper mod quisturpis nisi consequat etiam lorem. Phasellus quam turpis,
+														feugiat et sit amet ornare in, hendrerit in lectus semper mod quis eget mi dolore.</p>
+													</div>
+												</article>
+
+											<!-- Excerpt -->
+												<article class="box excerpt">
+													<a href="#" class="image left"><img src="images/pic06.jpg" alt="" /></a>
+													<div>
+														<header>
+															<span class="date">July 14</span>
+															<h3><a href="#">Buralar şimdilik Tarla</a></h3>
+														</header>
+														<p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus
+														semper mod quisturpis nisi consequat etiam lorem. Phasellus quam turpis,
+														feugiat et sit amet ornare in, hendrerit in lectus semper mod quis eget mi dolore.</p>
+													</div>
+												</article>
+										</section>
+									</div>
+									<div class="4u 12u(mobile)">
+
+										<!-- Spotlight -->
+											<section class="box spotlight">
+												<h2 class="icon fa-file-text-o">Spotlight</h2>
+												<article>
+													<a href="#" class="image featured"><img src="images/pic07.jpg" alt=""></a>
+													<header>
+														<h3><a href="#">Neural Implants</a></h3>
+														<p>The pros and cons. Mostly cons.</p>
+													</header>
+													<p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus semper mod
+													quisturpis nisi consequat ornare in, hendrerit in lectus semper mod quis eget mi quat etiam
+													lorem. Phasellus quam turpis, feugiat sed et lorem ipsum dolor consequat dolor feugiat sed
+													et tempus consequat etiam.</p>
+													<p>Lorem ipsum dolor quam turpis, feugiat sit amet ornare in, hendrerit in lectus semper
+													mod quisturpis nisi consequat etiam lorem sed amet quam turpis.</p>
+													<footer>
+														<a href="#" class="button alt icon fa-file-o">Continue Reading</a>
+													</footer>
+												</article>
+											</section>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Footer Wrapper -->
+				<div id="footer-wrapper">
+					<footer id="footer" class="container">
+						<div class="row">
+							<div class="3u 12u(mobile)">
+
+								<!-- Links -->
+									<section>
+										<h2>Filler Links</h2>
+										<ul class="divided">
+											<li><a href="#">Quam turpis feugiat dolor</a></li>
+											<li><a href="#">Amet ornare in hendrerit </a></li>
+											<li><a href="#">Semper mod quisturpis nisi</a></li>
+											<li><a href="#">Consequat etiam phasellus</a></li>
+										</ul>
+									</section>
+
+							</div>
+							<div class="3u 12u(mobile)">
+
+								<!-- Links -->
+									<section>
+										<h2>More Filler</h2>
+										<ul class="divided">
+											<li><a href="#">Quam turpis feugiat dolor</a></li>
+											<li><a href="#">Amet ornare in in lectus</a></li>
+											<li><a href="#">Semper mod sed tempus nisi</a></li>
+											<li><a href="#">Consequat etiam phasellus</a></li>
+										</ul>
+									</section>
+							</div>
+							<div class="6u 12u(mobile)">
+
+									<!-- Contact -->
+									<section>
+										<h2>Get in touch</h2>
+										<div>
+											<div class="row">
+												<div class="6u 12u(mobile)">
+													<dl class="contact">
+														<dt>Twitter</dt>
+														<dd><a href="#">@untitled-corp</a></dd>
+														<dt>Facebook</dt>
+														<dd><a href="#">facebook.com/untitled</a></dd>
+														<dt>WWW</dt>
+														<dd><a href="#">untitled.tld</a></dd>
+														<dt>Email</dt>
+														<dd><a href="#">kittencode@gmail.com</a></dd>
+													</dl>
+												</div>
+												<div class="6u 12u(mobile)">
+													<dl class="contact">
+														<dt>Address</dt>
+														<dd>
+															Celel Bayar University<br />
+															Muradiye Campus<br />
+															Manisa/ TURKIYE
+														</dd>
+														<dt>Phone</dt>
+														<dd>(551) 108-3553</dd>
+													</dl>
+												</div>
+											</div>
+										</div>
+									</section>
+
+							</div>
+						</div>
+						<div class="row">
+							<div class="12u">
+								<div id="copyright">
+									<ul class="menu">
+										<li>&copy;Kittencode 2015</li><li>Design: Kittencode Web Dev.</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</footer>
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
